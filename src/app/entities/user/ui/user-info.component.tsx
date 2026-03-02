99899b38f86a739019f008baa2ae04ffb4031496
Artiom Sviridovich <jsat1987@gmail.com>
2026-03-02 13:50:45 +0100

    little fix naming

--- src/app/entities/user/ui/user-info.component.tsx
+++ src/app/entities/user/ui/user-info.component.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic'
 
 const UserInfoView = dynamic(
-  () => import('./user-info.view').then((mod) => mod.UserInfoView),
+  () => import('./user-info-view').then((mod) => mod.UserInfoView),
   {
     ssr: false,
     loading: () => <p className="text-gray-600">Loading...</p>
